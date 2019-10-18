ec5988abcf7d54fb6d0c62865d9260f9783741af
Rituraj Jain <rituraj.jain17@gmail.com>
2019-10-18 17:03:49 +0530

    completed chapter-4

--- microblog.py
+++ microblog.py
@@ -1,4 +1,9 @@
-from blogapp import app
+from blogapp import app, db
+from blogapp.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+	return {'db': db, 'User': User, 'Post': Post}
 
 if __name__ == "__main__":
 	app.run(debug=True)
