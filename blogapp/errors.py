9bad98ac4d158b4141e1c0493fd094fbb3abb888
Rituraj Jain <rituraj.jain17@gmail.com>
2019-10-23 17:11:29 +0530

    Modified Login, Registration and Edit Profile Form using Bootstrap

--- blogapp/errors.py
+++ blogapp/errors.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from blog_app import app, db
+from blogapp import app, db
 
 @app.errorhandler(404)
 def not_found_error(error):
--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-		'sqlite:///' + os.path.join(basedir, 'app.db')
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-	# Details needed to send mail in case of errors
-	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    # Details needed to send mail in case of errors
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
